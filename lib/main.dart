5448f7a57db8f9c49e3b7803f17f45fc9eb6bb60
WilckerNeckel <wilckerwrsn@gmail.com>
2025-05-01 18:27:28 -0400

    feat: create base flutter project and set inital configurations in drift

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:project/database/database.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final database = AppDatabase();
+
+  await database.into(database.todoItems).insert(
+    TodoItemsCompanion.insert(
+      title: 'todo: finish drift setup',
+      content: 'We can now write queries and define our own tables.',
+    ),
+  );
+
+  runApp(MyApp(database: database));
+}
+
+class MyApp extends StatelessWidget {
+  final AppDatabase database;
+  const MyApp({super.key, required this.database});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(title: 'Drift Training', database: database),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final AppDatabase database;
+
+  const MyHomePage({
+    super.key,
+    required this.title,
+    required this.database,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<List<TodoItem>> _fetchItems() {
+    return widget.database.select(widget.database.todoItems).get();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Center(child: Text(widget.title)),
+      ),
+      body: FutureBuilder<List<TodoItem>>(
+        future: _fetchItems(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No clients found'));
+          }
+          final clients = snapshot.data!;
+          return ListView.builder(
+            padding: const EdgeInsets.all(16),
+            itemCount: clients.length,
+            itemBuilder: (context, index) {
+              final client = clients[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Text(
+                  client.content,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
