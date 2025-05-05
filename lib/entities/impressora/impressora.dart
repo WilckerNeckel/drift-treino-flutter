5954481326214ca55bbafbf8596cbf1159a134b5
WilckerNeckel <wilckerwrsn@gmail.com>
2025-05-05 15:13:49 -0400

    feat: create freezed impressora entity

---
+++ lib/entities/impressora/impressora.dart
@@ -0,0 +1,60 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'impressora.freezed.dart';
+@freezed
+class ImpressoraObj with _$ImpressoraObj {
+  const factory ImpressoraObj._({
+    String? id,
+    required String nome,
+    required String modelo,
+    required bool ativo,
+    required String tipoConexao,
+    required String ip,
+    required String porta,
+    required String tipoImpressao,
+  }) = _ImpressoraObj;
+
+  factory ImpressoraObj.fromDatabase(Map<String, dynamic> map) {
+    return ImpressoraObj._(
+      id: map['id'] as String?,
+      nome: map['nome'] as String,
+      modelo: map['modelo'] as String,
+      ativo: map['ativo'] as bool,
+      tipoConexao: map['tipoConexao'] as String,
+      ip: map['ip'] as String,
+      porta: map['porta'] as String,
+      tipoImpressao: map['tipoImpressao'] as String,
+    );
+  }
+
+  factory ImpressoraObj.create({
+    String? id,
+    required String nome,
+    required String modelo,
+    required bool ativo,
+    required String tipoConexao,
+    required String ip,
+    required String porta,
+    required String tipoImpressao,
+  }) {
+    if (nome.trim().isEmpty) throw ArgumentError('Nome é obrigatório');
+    if (modelo.trim().isEmpty) throw ArgumentError('Modelo é obrigatório');
+    if (!RegExp(r'^\d{1,5}$').hasMatch(porta)) {
+      throw ArgumentError('Porta inválida');
+    }
+    if (!RegExp(r'^(\d{1,3}\.){3}\d{1,3}$').hasMatch(ip)) {
+      throw ArgumentError('IP inválido');
+    }
+
+    return ImpressoraObj._(
+      id: id,
+      nome: nome,
+      modelo: modelo,
+      ativo: ativo,
+      tipoConexao: tipoConexao,
+      ip: ip,
+      porta: porta,
+      tipoImpressao: tipoImpressao,
+    );
+  }
+}
---
+++ lib/entities/impressora/impressora.freezed.dart
@@ -0,0 +1,290 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'impressora.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$ImpressoraObj {
+  String? get id => throw _privateConstructorUsedError;
+  String get nome => throw _privateConstructorUsedError;
+  String get modelo => throw _privateConstructorUsedError;
+  bool get ativo => throw _privateConstructorUsedError;
+  String get tipoConexao => throw _privateConstructorUsedError;
+  String get ip => throw _privateConstructorUsedError;
+  String get porta => throw _privateConstructorUsedError;
+  String get tipoImpressao => throw _privateConstructorUsedError;
+
+  /// Create a copy of ImpressoraObj
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $ImpressoraObjCopyWith<ImpressoraObj> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ImpressoraObjCopyWith<$Res> {
+  factory $ImpressoraObjCopyWith(
+          ImpressoraObj value, $Res Function(ImpressoraObj) then) =
+      _$ImpressoraObjCopyWithImpl<$Res, ImpressoraObj>;
+  @useResult
+  $Res call(
+      {String? id,
+      String nome,
+      String modelo,
+      bool ativo,
+      String tipoConexao,
+      String ip,
+      String porta,
+      String tipoImpressao});
+}
+
+/// @nodoc
+class _$ImpressoraObjCopyWithImpl<$Res, $Val extends ImpressoraObj>
+    implements $ImpressoraObjCopyWith<$Res> {
+  _$ImpressoraObjCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of ImpressoraObj
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? nome = null,
+    Object? modelo = null,
+    Object? ativo = null,
+    Object? tipoConexao = null,
+    Object? ip = null,
+    Object? porta = null,
+    Object? tipoImpressao = null,
+  }) {
+    return _then(_value.copyWith(
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String?,
+      nome: null == nome
+          ? _value.nome
+          : nome // ignore: cast_nullable_to_non_nullable
+              as String,
+      modelo: null == modelo
+          ? _value.modelo
+          : modelo // ignore: cast_nullable_to_non_nullable
+              as String,
+      ativo: null == ativo
+          ? _value.ativo
+          : ativo // ignore: cast_nullable_to_non_nullable
+              as bool,
+      tipoConexao: null == tipoConexao
+          ? _value.tipoConexao
+          : tipoConexao // ignore: cast_nullable_to_non_nullable
+              as String,
+      ip: null == ip
+          ? _value.ip
+          : ip // ignore: cast_nullable_to_non_nullable
+              as String,
+      porta: null == porta
+          ? _value.porta
+          : porta // ignore: cast_nullable_to_non_nullable
+              as String,
+      tipoImpressao: null == tipoImpressao
+          ? _value.tipoImpressao
+          : tipoImpressao // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ImpressoraObjImplCopyWith<$Res>
+    implements $ImpressoraObjCopyWith<$Res> {
+  factory _$$ImpressoraObjImplCopyWith(
+          _$ImpressoraObjImpl value, $Res Function(_$ImpressoraObjImpl) then) =
+      __$$ImpressoraObjImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String? id,
+      String nome,
+      String modelo,
+      bool ativo,
+      String tipoConexao,
+      String ip,
+      String porta,
+      String tipoImpressao});
+}
+
+/// @nodoc
+class __$$ImpressoraObjImplCopyWithImpl<$Res>
+    extends _$ImpressoraObjCopyWithImpl<$Res, _$ImpressoraObjImpl>
+    implements _$$ImpressoraObjImplCopyWith<$Res> {
+  __$$ImpressoraObjImplCopyWithImpl(
+      _$ImpressoraObjImpl _value, $Res Function(_$ImpressoraObjImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of ImpressoraObj
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? nome = null,
+    Object? modelo = null,
+    Object? ativo = null,
+    Object? tipoConexao = null,
+    Object? ip = null,
+    Object? porta = null,
+    Object? tipoImpressao = null,
+  }) {
+    return _then(_$ImpressoraObjImpl(
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String?,
+      nome: null == nome
+          ? _value.nome
+          : nome // ignore: cast_nullable_to_non_nullable
+              as String,
+      modelo: null == modelo
+          ? _value.modelo
+          : modelo // ignore: cast_nullable_to_non_nullable
+              as String,
+      ativo: null == ativo
+          ? _value.ativo
+          : ativo // ignore: cast_nullable_to_non_nullable
+              as bool,
+      tipoConexao: null == tipoConexao
+          ? _value.tipoConexao
+          : tipoConexao // ignore: cast_nullable_to_non_nullable
+              as String,
+      ip: null == ip
+          ? _value.ip
+          : ip // ignore: cast_nullable_to_non_nullable
+              as String,
+      porta: null == porta
+          ? _value.porta
+          : porta // ignore: cast_nullable_to_non_nullable
+              as String,
+      tipoImpressao: null == tipoImpressao
+          ? _value.tipoImpressao
+          : tipoImpressao // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$ImpressoraObjImpl implements _ImpressoraObj {
+  const _$ImpressoraObjImpl(
+      {this.id,
+      required this.nome,
+      required this.modelo,
+      required this.ativo,
+      required this.tipoConexao,
+      required this.ip,
+      required this.porta,
+      required this.tipoImpressao});
+
+  @override
+  final String? id;
+  @override
+  final String nome;
+  @override
+  final String modelo;
+  @override
+  final bool ativo;
+  @override
+  final String tipoConexao;
+  @override
+  final String ip;
+  @override
+  final String porta;
+  @override
+  final String tipoImpressao;
+
+  @override
+  String toString() {
+    return 'ImpressoraObj._(id: $id, nome: $nome, modelo: $modelo, ativo: $ativo, tipoConexao: $tipoConexao, ip: $ip, porta: $porta, tipoImpressao: $tipoImpressao)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ImpressoraObjImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.nome, nome) || other.nome == nome) &&
+            (identical(other.modelo, modelo) || other.modelo == modelo) &&
+            (identical(other.ativo, ativo) || other.ativo == ativo) &&
+            (identical(other.tipoConexao, tipoConexao) ||
+                other.tipoConexao == tipoConexao) &&
+            (identical(other.ip, ip) || other.ip == ip) &&
+            (identical(other.porta, porta) || other.porta == porta) &&
+            (identical(other.tipoImpressao, tipoImpressao) ||
+                other.tipoImpressao == tipoImpressao));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, id, nome, modelo, ativo,
+      tipoConexao, ip, porta, tipoImpressao);
+
+  /// Create a copy of ImpressoraObj
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ImpressoraObjImplCopyWith<_$ImpressoraObjImpl> get copyWith =>
+      __$$ImpressoraObjImplCopyWithImpl<_$ImpressoraObjImpl>(this, _$identity);
+}
+
+abstract class _ImpressoraObj implements ImpressoraObj {
+  const factory _ImpressoraObj(
+      {final String? id,
+      required final String nome,
+      required final String modelo,
+      required final bool ativo,
+      required final String tipoConexao,
+      required final String ip,
+      required final String porta,
+      required final String tipoImpressao}) = _$ImpressoraObjImpl;
+
+  @override
+  String? get id;
+  @override
+  String get nome;
+  @override
+  String get modelo;
+  @override
+  bool get ativo;
+  @override
+  String get tipoConexao;
+  @override
+  String get ip;
+  @override
+  String get porta;
+  @override
+  String get tipoImpressao;
+
+  /// Create a copy of ImpressoraObj
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$ImpressoraObjImplCopyWith<_$ImpressoraObjImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
